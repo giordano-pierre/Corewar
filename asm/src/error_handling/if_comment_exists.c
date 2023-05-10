b3d2108f6687dc9c5f5a189cf5bab149cff3743a
N'da gnanmienlie patrick-levy <gnanmienlie-patrick-levy.n-da@epitech.eu>
2023-05-10 10:15:14 +0200

    [UPD] if_comment_exists function to add new features

--- asm/src/error_handling/if_comment_exists.c
+++ asm/src/error_handling/if_comment_exists.c
@@ -11,16 +11,21 @@ int if_comment_exists(char **tab)
 {
     char **tab_bis = my_str_to_word_array(tab[1]);
 
+    int len_tab = my_arraylen(tab_bis);
+    if (len_tab != 2) {
+        return 1;
+    }
+
     if (tab_bis[0] != NULL) {
-        if (my_strcmp(tab_bis[1], COMMENT_CMD_STRING) == 0) {
-            return -1;
+        if (my_strcmp(tab_bis[0], COMMENT_CMD_STRING) != 0) {
+            return 1;
         }
     }
 
     int len_comment = my_strlen(tab_bis[1]);
 
-    if (len_comment > COMMENT_LENGTH) {
-        return -1;
+    if ((len_comment - 2) > COMMENT_LENGTH) {
+        return 1;
     }
     return 0;
 }
