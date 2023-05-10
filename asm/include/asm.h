50f9aaf0474872d08de9dbc098a5184dd3c72f51
Nathan baudelin <nathan.baudelin@epitech.eu>
2023-05-10 10:56:26 +0200

    [ADD] new functions

--- asm/include/asm.h
+++ asm/include/asm.h
@@ -8,6 +8,7 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include "../../op.h"
 #include "../../lib/printf/include/my_printf.h"
 
 #ifndef ASM_H_
@@ -27,13 +28,18 @@
     char **add_line(char **array, char *line);
     void print_array(char **array);
     void free_array(char **array);
-    int my_arraylen (char **array);
+    int my_arraylen(char **array);
 
     //my_str_to_word_array.c
     char **my_str_to_word_array(char const *str);
     int count_word (char const *str);
     int count_char(char const *str, int i);
+    void skip_quote(char const *str, int *ind);
+    int have_second_quote(char const *str, int ind);
 
-    ssize_t my_strlen_long(char const *str);
+    void my_asm(char **file, char *filepath);
+    char *name_file_point_core(char *file_name);
+    void write_instruction(char **file, FILE *fd);
+    int inverse_magic(int i);
 
 #endif /* !ASM_H_ */
