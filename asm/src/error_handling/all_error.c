a0d8b001c8636a08bb536925d1d6494604834695
N'da gnanmienlie patrick-levy <gnanmienlie-patrick-levy.n-da@epitech.eu>
2023-05-10 10:39:15 +0200

    [RMV] error messages

--- asm/src/error_handling/all_error.c
+++ asm/src/error_handling/all_error.c
@@ -10,11 +10,9 @@
 int all_error(char **tab)
 {
     if (if_name_exists(tab) != 0) {
-        write(2, ".name doesn't exist or the name is too long\n", 44);
         return -1;
     }
     if (if_comment_exists(tab) != 0) {
-        write(2, ".comment doesn't exist or the comment is too long\n", 50);
         return -1;
     }
     return 0;
