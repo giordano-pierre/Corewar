3041bf7edd9698220c1ea21ae08ff196c4bab174
Nathan baudelin <nathan.baudelin@epitech.eu>
2023-05-14 18:31:37 +0200

    [ADD] file for tools

---
+++ asm/src/tools.c
@@ -0,0 +1,37 @@
+/*
+** EPITECH PROJECT, 2023
+** B-CPE-200-LYN-2-1-corewar-pierre.giordano
+** File description:
+** tools
+*/
+#include "../include/asm.h"
+
+int find_double_point(char *str)
+{
+    for (int i = 0; str[i]; i++) {
+        if (str[i] == ':')
+            return 0;
+    }
+    return -1;
+}
+
+int	my_getnbr_ind(char const *str, int indice)
+{
+    int	nb = 0;
+    int	signe = 1;
+
+    while (str[indice] == '+' || str[indice] == '-'){
+        if (str[indice] == '-')
+            signe = signe * (-1);
+        indice = indice + 1;
+    }
+    while (str[indice] != '\0'){
+        if (str[indice] >= '0' && str[indice] <= '9'){
+            nb = nb * 10;
+            nb = nb + str[indice] - '0';
+            indice = indice + 1;
+        } else
+            return (nb * signe);
+    }
+    return (nb * signe);
+}
