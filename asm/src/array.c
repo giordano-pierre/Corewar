b7c5e772af39b3c806b4a8b02cae5cab81bfdd5b
Nathan baudelin <nathan.baudelin@epitech.eu>
2023-05-14 18:24:37 +0200

    [UPD] move function + add new functions 'add_line'

--- asm/src/array.c
+++ asm/src/array.c
@@ -6,26 +6,6 @@
 */
 #include "../include/asm.h"
 
-void free_array(char **array)
-{
-    if (array == NULL)
-        return;
-    for (int i = 0; array[i] != NULL; i++) {
-        free(array[i]);
-    }
-    free(array);
-}
-
-void print_array(char **array)
-{
-    if (array == NULL)
-        return;
-    int i = 0;
-    for (; array[i] != NULL; i++) {
-        write(1, array[i], my_strlen(array[i]));
-    }
-}
-
 char **add_line(char **array, char *line)
 {
     int size = 0;
@@ -42,10 +22,64 @@ char **add_line(char **array, char *line)
     return new;
 }
 
+int my_structlen(instruct_t **info)
+{
+    int size = 0;
+
+    for (int i = 0; info[i] != NULL; i++) {
+        size = size + info[i]->size;
+        if (info[i]->bin != 0)
+            size++;
+    }
+    return size;
+}
+
 int my_arraylen(char **array)
 {
     int row = 0;
+
     for (int i = 0; array[i] != NULL; i++)
         row++;
     return row;
+}
+
+int add_instr(asm_t *my_asm, int code, int inst, int size)
+{
+    int len = 0;
+    instruct_t **new = NULL;
+
+    while (my_asm->info[len] != NULL)
+        len++;
+    new = malloc(sizeof(instruct_t *) * (len + 2));
+    for (int i = 0; i < len; i++)
+        new[i] = my_asm->info[i];
+    new[len] = malloc(sizeof(instruct_t));
+    new[len]->code = code;
+    new[len]->inst = inst;
+    new[len]->size = size;
+    new[len]->label = my_asm->old_label;
+    new[len]->bin = 0;
+    my_asm->old_label = NULL;
+    new[len + 1] = NULL;
+    free(my_asm->info);
+    my_asm->info = new;
+    return len;
+}
+
+label_t **add_label(label_t **labels, char *name, int nb)
+{
+    int len = 0;
+    label_t **new = NULL;
+
+    while (labels[len] != NULL)
+        len++;
+    new = malloc(sizeof(label_t *) * (len + 2));
+    for (int i = 0; i < len; i++)
+        new[i] = labels[i];
+    new[len] = malloc(sizeof(label_t));
+    new[len]->name = name;
+    new[len]->nb = nb;
+    new[len + 1] = NULL;
+    free(labels);
+    return new;
 }
