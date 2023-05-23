cefd9c1644de90650e9466cb43aa81121a04a914
giordano <pierre.giordano@epitech.eu>
2023-05-23 09:11:24 +0200

    [UPD] good -h corewar

--- corewar/src/main.c
+++ corewar/src/main.c
@@ -35,13 +35,14 @@ int bad_enter(char **file)
 
 int main(int ac, char **av)
 {
+    if (ac == 2)
+        if (my_strcmp(av[1], "-h") == 0) {
+            print_h();
+            return 0;
+        }
     if (ac < 3) {
         write(2, "corewar: Bad number of champions.\n", 34);
         return 84;
-    }
-    if (my_strcmp(av[1], "-h") == 0) {
-        print_h();
-        return 0;
     }
     char **file = read_file(av[1]);
     if (bad_enter(file))
