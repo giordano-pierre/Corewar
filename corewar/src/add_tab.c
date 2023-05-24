cad1b831aad5b97fd0f3eb7101bb8a0b885c296c
Nathan baudelin <nathan.baudelin@epitech.eu>
2023-05-24 16:07:12 +0200

    [FIX] segfault in add_tab

--- corewar/src/add_tab.c
+++ corewar/src/add_tab.c
@@ -10,21 +10,21 @@
 champ_t **add_tab(champ_t **warriors, int adress, char *file, int nb_p)
 {
     int taille = 0;
-    while (warriors[taille] != NULL) {
+    champ_t **new_warriors = NULL;
+
+    while (warriors[taille] != NULL)
         taille++;
-    }
-    champ_t **new_warriors = malloc((taille + 2) * sizeof(champ_t *));
-    new_warriors[0] = sizeof(champ_t *);
+    new_warriors = malloc(sizeof(champ_t *) * (taille + 2));
+    new_warriors[0] = malloc(sizeof(champ_t));
     new_warriors[0]->adress = adress;
     new_warriors[0]->code = NULL;
     new_warriors[0]->comment = NULL;
-    new_warriors[0]->file = file;
+    new_warriors[0]->file = my_strdup(file);
     new_warriors[0]->name = NULL;
     new_warriors[0]->nb_prog = nb_p;
     new_warriors[0]->pc = 0;
-    for (int i = 0; i < taille; i++) {
+    for (int i = 0; i < taille; i++)
         new_warriors[i + 1] = warriors[i];
-    }
     new_warriors[taille + 1] = NULL;
     free(warriors);
     return new_warriors;
