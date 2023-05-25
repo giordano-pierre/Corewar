7e09bf9d3e4d8e9d55fd86fdc03c5e955376db5e
giordano <pierre.giordano@epitech.eu>
2023-05-25 11:05:32 +0200

    [UPD] ld whith IDX_MOD

--- corewar/src/intructions/ld.c
+++ corewar/src/intructions/ld.c
@@ -14,18 +14,15 @@ void direct_load_fonction(corewar_t *corewar, champ_t *champion)
     int encoding = (unsigned char)corewar->mem[champion->pc + 1];
 
     if (encoding == T_DIR) {
-        value = read_memory_value(corewar->mem, champion->pc + 2, 4);
+        value = read_memory_value(corewar->mem, champion->pc + 2, DIR_SIZE);
     } else if (encoding == T_IND) {
-        int address = read_memory_value(corewar->mem, champion->pc + 2, 2);
-        value = read_memory_value(corewar->mem, address, 4);
+        int address = read_memory_value(corewar->mem,
+            champion->pc + 2, IND_SIZE);
+        address %= IDX_MOD;
+        value = read_memory_value(corewar->mem, address, DIR_SIZE);
     }
-    reg_num = (unsigned char)corewar->mem[champion->pc + 6];
-    if (reg_num >= 1 && reg_num <= champion->nb_prog) {
-        int reg_address = (reg_num - 1) * 4;
-        for (int i = 0; i < 4; i++) {
-            champion->code[reg_address + i] = (value >> ((3 - i) * 8)) & 0xFF;
-        }
-        champion->carry = (value == 0) ? 1 : 0;
-    }
-    champion->pc += 7;
+    reg_num = (unsigned char)corewar->mem[champion->pc + 2 +
+        encoding == T_DIR ? DIR_SIZE : IND_SIZE];
+    update_register(reg_num, value, champion);
+    champion->pc += 2 + encoding == T_DIR ? DIR_SIZE : IND_SIZE + 1;
 }
