9911500b17d989a2f7ea2fa4284f264b4ff5af04
giordano <pierre.giordano@epitech.eu>
2023-05-25 17:58:04 +0200

    [UPD] finish ld v3

--- corewar/src/intructions/ld.c
+++ corewar/src/intructions/ld.c
@@ -10,16 +10,18 @@ void direct_load_fonction(corewar_t *corewar, champ_t *champion)
 {
     int value;
     int reg_num;
-    int encoding2 = (unsigned char)corewar->mem[(champion->pc + 1) % MEM_SIZE];
+    int encoding_byte = (unsigned char)corewar->mem[(champion->pc + 1) %
+        MEM_SIZE];
+    int arg_type = (encoding_byte >> 6) & 0b11;
 
-    if (encoding2 == T_DIR) {
-        value = read_memory_value(corewar->mem,
-            (champion->pc + 2) % MEM_SIZE, DIR_SIZE);
+    if (arg_type == T_DIR) {
+        value = read_memory_value(corewar->mem, (champion->pc + 2) %
+            MEM_SIZE, DIR_SIZE);
         reg_num = (unsigned char)corewar->mem[(champion->pc + 6) % MEM_SIZE];
         champion->pc += 7;
-    } else if (encoding2 == T_IND) {
-        int address = read_memory_value(corewar->mem,
-            (champion->pc + 2) % MEM_SIZE, IND_SIZE);
+    } else if (arg_type == T_IND) {
+        int address = read_memory_value(corewar->mem, (champion->pc + 2) %
+            MEM_SIZE, IND_SIZE);
         address = (champion->pc + (address % IDX_MOD)) % MEM_SIZE;
         value = read_memory_value(corewar->mem, address, REG_SIZE);
         reg_num = (unsigned char)corewar->mem[(champion->pc + 4) % MEM_SIZE];
