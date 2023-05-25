e6a5a3a7043d4b6db276805db599f5a9c2cb4591
giordano <pierre.giordano@epitech.eu>
2023-05-25 18:09:36 +0200

    [UPD] good final lld

--- corewar/src/intructions/lld.c
+++ corewar/src/intructions/lld.c
@@ -10,20 +10,22 @@ void direct_long_load_fonction(corewar_t *corewar, champ_t *champion)
 {
     int value;
     int reg_num;
-    int arg_type = (unsigned char)corewar->mem[(champion->pc + 1) % MEM_SIZE];
+    int encoding_byte = (unsigned char)corewar->mem[(champion->pc +
+        1) % MEM_SIZE];
+    int arg_type = (encoding_byte >> 6) & 0b11;
 
     if (arg_type == T_DIR) {
-        value = read_memory_value(corewar->mem,
-            (champion->pc + 2) % MEM_SIZE, DIR_SIZE);
+        value = read_memory_value(corewar->mem, (champion->pc + 2) %
+            MEM_SIZE, DIR_SIZE);
         reg_num = (unsigned char)corewar->mem[(champion->pc + 6) % MEM_SIZE];
         champion->pc += 7;
     } else if (arg_type == T_IND) {
-        int address = read_memory_value(corewar->mem,
-            (champion->pc + 2) % MEM_SIZE, IND_SIZE);
-        value = read_memory_value(corewar->mem,
-            (champion->pc + address) % MEM_SIZE, REG_SIZE);
-        reg_num = (unsigned char)corewar->mem[(champion->pc + 2 + IND_SIZE) %
-            MEM_SIZE];
+        int address = read_memory_value(corewar->mem, (champion->pc + 2) %
+            MEM_SIZE, IND_SIZE);
+        address = (champion->pc + address) % MEM_SIZE;
+        value = read_memory_value(corewar->mem, address, REG_SIZE);
+        reg_num = (unsigned char)corewar->mem[(champion->pc + 2 +
+            IND_SIZE) % MEM_SIZE];
         champion->pc += 3 + IND_SIZE;
     }
     update_register(reg_num, value, champion);
