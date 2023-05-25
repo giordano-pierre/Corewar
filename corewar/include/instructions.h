037618251a358c33eaaa98bd7fd1d0e8cd0d08ad
giordano <pierre.giordano@epitech.eu>
2023-05-25 14:48:31 +0200

    [UPD] add ldi + read register in .h

--- corewar/include/instructions.h
+++ corewar/include/instructions.h
@@ -12,16 +12,24 @@
     //intructions/
     // memory_value.c
     int read_memory_value(char *memory, int address, int size);
-    //ld.c
-    void direct_load_fonction(corewar_t *corewar, champ_t *champion);
-    //lld.c
-    void direct_long_load_fonction(corewar_t *corewar, champ_t *champion);
-    //ldi.c
-    void indirect_load_fonction(corewar_t *corewar, champ_t *champion);
+
+    //update_register.c
+    void update_register(int reg_num, int value, champ_t *champion);
+
+    //read_register.c
+    int read_register(char *code, int reg_num);
+
     //live.c
     int live_fonction(champ_t *champion, corewar_t *corewar);
     int get_adrr(champ_t *champion);
-    //update_register.c
-    void update_register(int reg_num, int value, champ_t *champion);
+
+    //ld.c
+    void direct_load_fonction(corewar_t *corewar, champ_t *champion);
+
+    //lld.c
+    void direct_long_load_fonction(corewar_t *corewar, champ_t *champion);
+
+    //ldi.c
+    void indirect_load_fonction(corewar_t *corewar, champ_t *champion);
 
 #endif /* !INSTRUCTIONS_H_ */
