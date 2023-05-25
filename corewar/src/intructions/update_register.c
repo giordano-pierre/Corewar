7ec9e7d71a28c444845cfe7ce2dc7b2ec9708bef
giordano <pierre.giordano@epitech.eu>
2023-05-25 11:22:56 +0200

    [UPD] regsiter cs

--- corewar/src/intructions/update_register.c
+++ corewar/src/intructions/update_register.c
@@ -12,7 +12,8 @@ void update_register(int reg_num, int value, champ_t *champion)
     if (reg_num >= 1 && reg_num <= REG_NUMBER) {
         int reg_address = (reg_num - 1) * REG_SIZE;
         for (int i = 0; i < REG_SIZE; i++) {
-            champion->code[reg_address + i] = (value >> ((REG_SIZE - 1 - i) * 8)) & 0xFF;
+            champion->code[reg_address + i] =
+            (value >> ((REG_SIZE - 1 - i) * 8)) & 0xFF;
         }
         champion->carry = (value == 0) ? 1 : 0;
     }
