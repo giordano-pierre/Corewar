11eb94871a1cf0b1abd6ee63ee5805dc71eeedff
giordano <pierre.giordano@epitech.eu>
2023-05-25 11:07:05 +0200

    [ADD] ldi.c v1

---
+++ corewar/src/intructions/ldi.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2023
+** Corewar
+** File description:
+** ldi
+*/
+#include "../../include/corewar.h"
+#include "../../include/struct.h"
+
+void indirect_load_fonction(corewar_t *corewar, champ_t *champion)
+{
+
+}
