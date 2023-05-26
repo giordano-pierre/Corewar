87ee9e55005934840477d83f04771e3385bc09fc
N'da gnanmienlie patrick-levy <gnanmienlie-patrick-levy.n-da@epitech.eu>
2023-05-26 11:44:48 +0200

    [UPD] recup_flags function to fix the coding style errors

--- corewar/src/flags_management/recup_flags.c
+++ corewar/src/flags_management/recup_flags.c
@@ -12,10 +12,8 @@ int recup_flags(char **av, corewar_t *corewar)
     corewar->dump = -1; int nb_f = 0;
     int prog = -1; int adress = 0;
     for (int i = 1; av[i] != NULL; i++) {
-        if (flags_error(av, i) != 0)
-            return 1;
-        if (dump_flag(av, corewar, &i) != 0)
-            continue;
+        if (flags_error(av, i) != 0) return 1;
+        if (dump_flag(av, corewar, &i) != 0) continue;
         prog = prog_number_flag(av, i, prog);
         if (if_prog_number_exists(av, &i))
             continue;
