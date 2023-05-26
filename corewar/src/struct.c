a80ae931a9fbf066f13aadfaca67acda6523017c
Nathan baudelin <nathan.baudelin@epitech.eu>
2023-05-26 10:55:10 +0200

    [UPD] add new var

--- corewar/src/struct.c
+++ corewar/src/struct.c
@@ -12,11 +12,13 @@ corewar_t *create_base(void)
 
     corewar->cycle_to_die = CYCLE_TO_DIE;
     corewar->dump = -1;
-    corewar->last_live = NULL;
+    corewar->last_live = -1;
     corewar->mem = NULL;
     corewar->nbr_live = NBR_LIVE;
     corewar->warriors = malloc(sizeof(champ_t *));
     corewar->warriors[0] = NULL;
+    corewar->nb_live = 0;
+    corewar->nb_cycle = 0;
     return corewar;
 }
 
