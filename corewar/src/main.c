0e8bc9329bda3b6990762191bdd37dc55dc07e88
N'da gnanmienlie patrick-levy <gnanmienlie-patrick-levy.n-da@epitech.eu>
2023-05-26 11:06:10 +0200

    [UPD] main function by adding recup_flags function

--- corewar/src/main.c
+++ corewar/src/main.c
@@ -11,12 +11,12 @@ void print_h(void)
     my_printf("USAGE\n./corewar [-dump nbr_cycle] ");
     my_printf("[[-n prog_number] [-a load_address] prog_name] ...\n");
     my_printf("DESCRIPTION\n-dump nbr_cycle dumps the memory ");
-    my_printf("after the nbr_cycle execution (if the round isn’t\n");
+    my_printf("after the nbr_cycle execution (if the round isn't\n");
     my_printf("already over) with the following format: 32 bytes/line\n");
     my_printf("in hexadecimal (A0BCDEFE1DD3...)\n");
-    my_printf("-n prog_number sets the next program’s number.");
+    my_printf("-n prog_number sets the next program's number.");
     my_printf(" By default, the first free number\nin the parameter order\n");
-    my_printf("-a load_address sets the next program’s loading address.");
+    my_printf("-a load_address sets the next program's loading address.");
     my_printf(" When no address is\nspecified, optimize the addresses");
     my_printf(" so that the processes are as far\naway from each other");
     my_printf(" as possible. The addresses are MEM_SIZE modulo.\n");
@@ -44,10 +44,11 @@ int main(int ac, char **av)
     if ((err = verif_arg(ac, av)) != -1)
         return err;
     base = create_base();
+    if (recup_flags(av, base) != 0)
+        return 84;
     base->warriors = create_champ(base->warriors);
     fill_champ(base->warriors);
     print_champ(base->warriors);
     free_base(base);
     return 0;
 }
-    // function Patoche a la place de create_champ()
