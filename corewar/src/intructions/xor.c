64bec1f622ea33bf01f634b5e275c3dc3133b4f3
axel petitjean <axel.petitjean@epitech.eu>
2023-05-26 18:10:36 +0200

    [ADD] xor function

---
+++ corewar/src/intructions/xor.c
@@ -0,0 +1,28 @@
+/*
+** EPITECH PROJECT, 2023
+** Corewar
+** File description:
+** xor
+*/
+
+#include "../../include/corewar.h"
+
+void xor_function(champ_t *champion, corewar_t *corewar)
+{
+    int adress;
+    int encod_byt = (unsigned char)corewar->mem[(champion->pc + 1) % MEM_SIZE];
+    int arg1_type = (encod_byt >> 6) & 0b11;
+    int arg2_type = (encod_byt >> 4) & 0b11;
+    argument_t arg1 = {corewar->mem, champion->pc + 2, arg1_type,
+        &adress, champion};
+    argument_t arg2 = {corewar->mem, champion->pc + 2 + arg1.address,
+        arg2_type, &adress, champion};
+    int frst_arg = read_argument_value(&arg1);
+    int secnd_arg = read_argument_value(&arg2);
+    int res = frst_arg ^ secnd_arg;
+    if (res == 0) champion->carry = 1;
+    else
+        champion->carry = 0;
+    adress = (champion->pc + ((frst_arg + secnd_arg) % IDX_MOD)) % MEM_SIZE;
+    champion->pc += 1 + adress;
+}
