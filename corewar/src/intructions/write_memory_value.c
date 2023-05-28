4c012288f6c5b66106f957c501cafb4cacf45eb8
giordano <pierre.giordano@epitech.eu>
2023-05-28 19:44:23 +0200

    [UPD] new write mem val

--- corewar/src/intructions/write_memory_value.c
+++ corewar/src/intructions/write_memory_value.c
@@ -2,11 +2,11 @@
 ** EPITECH PROJECT, 2023
 ** Corewar
 ** File description:
-** write_memory_value
+** write_mem
 */
 #include "../../include/corewar.h"
 
-void write_memory_value(unsigned char *memory, int address, int size, int value)
+void write_mem(unsigned char *memory, int address, int size, int value)
 {
     for (int i = size - 1; i >= 0; i--) {
         memory[(address + i) % MEM_SIZE] = (value >> (8 * i)) & 0xFF;
