1736036141a0c749d77b14467b7195829e8c7f55
giordano <pierre.giordano@epitech.eu>
2023-05-29 18:07:39 +0200

    [ADD] lfork.c

---
+++ corewar/src/intructions/lfork.c
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2023
+** Corewar
+** File description:
+** lfork
+*/
+#include "../../include/corewar.h"
+
+void lfork_fonction(corewar_t *corewar, champ_t *champion)
+{
+    int offset = read_memory_value(corewar->mem, (champion->pc + 1) %
+        MEM_SIZE, IND_SIZE);
+    int new_pc = (champion->pc + offset) % MEM_SIZE;
+    champ_t *new_champion = create_champion();
+
+    copy_champion_state(champion, new_champion, new_pc);
+    add_warrior(corewar, new_champion);
+    load_champ_fork(corewar->mem, new_champion);
+    new_champion->pc += 3;
+}
