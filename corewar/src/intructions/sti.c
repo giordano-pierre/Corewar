896bb67d27f4a2694a45fa736b64a23e2d828356
axel petitjean <axel.petitjean@epitech.eu>
2023-05-29 18:48:40 +0200

    [UPD] function sti

--- corewar/src/intructions/sti.c
+++ corewar/src/intructions/sti.c
@@ -6,55 +6,18 @@
 */
 #include "../../include/corewar.h"
 
-int arg_val(unsigned char *memory, int address, int arg_type,
-    champ_t *champion)
-{
-    int value = 0;
-
-    if (arg_type == T_REG) {
-        int reg_num = (unsigned char)memory[address % MEM_SIZE];
-        value = champion->reg[reg_num - 1];
-    }
-    if (arg_type == T_DIR)
-        value = read_memory_value(memory, address % MEM_SIZE, DIR_SIZE);
-    if (arg_type == T_IND) {
-        int offset = (short)read_memory_value(memory, address %
-            MEM_SIZE, IND_SIZE);
-        int effective_address = (champion->pc + offset) % MEM_SIZE;
-        value = read_memory_value(memory, effective_address, REG_SIZE);
-    }
-    return value;
-}
-
-int get_size(int arg_type)
-{
-    if (arg_type == T_REG)
-        return 1;
-    if (arg_type == T_DIR)
-        return DIR_SIZE;
-    if (arg_type == T_IND)
-        return IND_SIZE;
-    return 0;
-}
-
 void indirect_store(corewar_t *corewar, champ_t *champion)
 {
-    int reg_src_num;
-    int encoding_byte = corewar->mem[(champion->pc + 1) % MEM_SIZE];
-    int arg1_type = (encoding_byte >> 6) & 0b11;
-    int arg2_type = (encoding_byte >> 4) & 0b11;
-    int arg3_type = (encoding_byte >> 2) & 0b11;
-
-    if (arg1_type == T_REG) {
-        reg_src_num = corewar->mem[(champion->pc + 2) % MEM_SIZE];
-    } else
-        return;
-    int val2 = arg_val(corewar->mem, champion->pc + 2 + get_size(arg2_type),
-        arg3_type, champion);
-    int val3 = arg_val(corewar->mem, champion->pc + 2 + get_size(arg2_type)
-        + get_size(arg3_type), T_REG, champion);
-    int address = (champion->pc + ((val2 + val3) % IDX_MOD)) % MEM_SIZE;
-    write_mem(corewar->mem, address, REG_SIZE, champion->reg[reg_src_num - 1]);
-    champion->pc += 2 + get_size(arg2_type) +
-        get_size(arg3_type) + get_size(T_REG);
+    int pc_size = 3;
+    int num_reg = corewar->mem[(champion->pc + 2) % MEM_SIZE];
+    int res = champion->reg[num_reg - 1];
+    int var_1 = get_param_value_bis(corewar, champion, 1, &pc_size);
+    int var_2 = get_param_value_bis(corewar, champion, 2, &pc_size);
+    int adress = (champion->pc + ((var_1 + var_2) % IDX_MOD)) % MEM_SIZE;
+    write_mem(corewar->mem, adress, 4, res);
+    if (num_reg == 0)
+        champion->carry = 1;
+    else
+        champion->carry = 0;
+    champion->pc = (champion->pc + pc_size) % MEM_SIZE;
 }
