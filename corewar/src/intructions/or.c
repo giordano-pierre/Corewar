c3eeb77ed53f32f7fdec5689ade5754a039432aa
giordano <pierre.giordano@epitech.eu>
2023-05-29 10:41:09 +0200

    [UPD] final or

--- corewar/src/intructions/or.c
+++ corewar/src/intructions/or.c
@@ -11,9 +11,9 @@ void or_function(corewar_t *corewar, champ_t *champion)
 {
     if (good_arg(corewar, champion))
         return;
-    int reg_dst_num = corewar->mem[(champion->pc + 2) % MEM_SIZE];
-    int reg_src1_num = corewar->mem[(champion->pc + 2 + 1) % MEM_SIZE];
-    int reg_src2_num = corewar->mem[(champion->pc + 2 + 1 + 1) % MEM_SIZE];
+    int reg_src1_num = corewar->mem[(champion->pc + 2) % MEM_SIZE];
+    int reg_src2_num = corewar->mem[(champion->pc + 3) % MEM_SIZE];
+    int reg_dst_num = corewar->mem[(champion->pc + 4) % MEM_SIZE];
     if (reg_dst_num >= 1 && reg_dst_num <= REG_NUMBER &&
         reg_src1_num >= 1 && reg_src1_num <= REG_NUMBER &&
         reg_src2_num >= 1 && reg_src2_num <= REG_NUMBER) {
